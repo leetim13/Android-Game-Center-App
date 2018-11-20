54bc110bd78b6ad9f4ada15fccc9914395ad5a36
liquanzh <quanzhou.li@mail.utoronto.ca>
2018-11-20 11:53:36 -0500

    add BasicTile javadoc

--- Phase2/GameCenter/app/src/main/java/fall2018/csc2017/slidingtiles/component/BasicTile.java
+++ Phase2/GameCenter/app/src/main/java/fall2018/csc2017/slidingtiles/component/BasicTile.java
@@ -8,9 +8,24 @@ import java.io.Serializable;
 * abstract of the tiles, standard method
 * */
 public abstract class BasicTile implements Comparable <BasicTile>, Serializable {
+    /**
+     * The id of the BasicTile
+     */
     public int id;
+
+    /**
+     * Return the id of the BasicTile
+     * @return the id of the BasicTile
+     */
     public abstract int getId();
 
+    /**
+     * Calculate the difference between the id of this BasicTile and
+     * the id of the other BasicTile o
+     * @param o the other BasicTile
+     * @return the difference between the id of this BasicTile and
+     *          the id of the other BasicTile o
+     */
     @Override
     public int compareTo(@NonNull BasicTile o) {
         return o.id - this.id;
