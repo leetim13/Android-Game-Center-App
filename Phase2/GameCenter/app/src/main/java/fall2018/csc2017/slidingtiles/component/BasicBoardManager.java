4a2a37636f52dae23ad86f492cae0fb5170882bc
liquanzh <quanzhou.li@mail.utoronto.ca>
2018-11-20 11:49:31 -0500

    add BasicBoardManager javadoc

--- Phase2/GameCenter/app/src/main/java/fall2018/csc2017/slidingtiles/component/BasicBoardManager.java
+++ Phase2/GameCenter/app/src/main/java/fall2018/csc2017/slidingtiles/component/BasicBoardManager.java
@@ -1,9 +1,31 @@
 package fall2018.csc2017.slidingtiles.component;
 
 public abstract class BasicBoardManager {
+    /**
+     * Add the score in the BasicBoardManager by some number
+     */
     public abstract void addScore();
+
+    /**
+     * Minus the score in the BasicBoardManager by some number
+     */
     public abstract void minusScore();
+
+    /**
+     * Return the score in the BasicBoardManager
+     * @return the score in the BasicBoardManager
+     */
     public abstract int getScore();
+
+    /**
+     * Return the number of columns in the board of the BasicBoardManager
+     * @return the number of columns in the board of the BasicBoardManager
+     */
     public abstract int getBoardNumOfCols();
+
+    /**
+     * Return the number of rows in the board of the BasicBoardManager
+     * @return the number of rows in the board of the BasicBoardManager
+     */
     public abstract int getBoardNumOfRows();
 }
