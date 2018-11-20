01156afb1532cfa9d0e7846801eaa749db5cf8c2
liquanzh <quanzhou.li@mail.utoronto.ca>
2018-11-20 00:25:09 -0500

    leftOperation test expression modification

--- Phase2/GameCenter/app/src/test/java/fall2018/csc2017/slidingtiles/TestsOfGame2048/BoardManegerTFTest.java
+++ Phase2/GameCenter/app/src/test/java/fall2018/csc2017/slidingtiles/TestsOfGame2048/BoardManegerTFTest.java
@@ -12,28 +12,28 @@ public class BoardManegerTFTest {
     private BoardManagerTF boardManagerTF;
 
     private BoardManagerTF setBoardManagerTF(){
-        BoardManagerTF boardManagerTF = new BoardManagerTF(BoardTF.LENGTH_OF_SIDE);
+        boardManagerTF = new BoardManagerTF(BoardTF.LENGTH_OF_SIDE);
         return boardManagerTF;
     }
 
     @Test
     public void testLeftOperationSimple(){
         boardManagerTF = setBoardManagerTF();
-        ((TfTile)boardManagerTF.getBoardTF().getTile(1, 1)).setId(1);
-        ((TfTile)boardManagerTF.getBoardTF().getTile(1, 3)).setId(1);
-        boardManagerTF.leftOperation();
+        boardManagerTF.getBoardTF().getTile(1, 1).setId(1);
+        boardManagerTF.getBoardTF().getTile(1, 3).setId(1);
+        boardManagerTF.touchMove('l');
         assertEquals(2, (boardManagerTF.getBoardTF().getTile(1, 0)).getId());
     }
 
     @Test
     public void testLeftOperationComplex(){
         boardManagerTF = setBoardManagerTF();
-        ((TfTile)boardManagerTF.getBoardTF().getTile(0, 3)).setId(1);
-        ((TfTile)boardManagerTF.getBoardTF().getTile(2, 0)).setId(1);
-        ((TfTile)boardManagerTF.getBoardTF().getTile(2, 3)).setId(3);
-        ((TfTile)boardManagerTF.getBoardTF().getTile(3, 2)).setId(2);
-        ((TfTile)boardManagerTF.getBoardTF().getTile(3, 3)).setId(3);
-        boardManagerTF.leftOperation();
+        boardManagerTF.getBoardTF().getTile(0, 3).setId(1);
+        boardManagerTF.getBoardTF().getTile(2, 0).setId(1);
+        boardManagerTF.getBoardTF().getTile(2, 3).setId(3);
+        boardManagerTF.getBoardTF().getTile(3, 2).setId(2);
+        boardManagerTF.getBoardTF().getTile(3, 3).setId(3);
+        boardManagerTF.touchMove('l');
         assertEquals(1, (boardManagerTF.getBoardTF().getTile(0, 0)).getId());
         assertEquals(1, (boardManagerTF.getBoardTF().getTile(2, 0)).getId());
         assertEquals(3, (boardManagerTF.getBoardTF().getTile(2, 1)).getId());
