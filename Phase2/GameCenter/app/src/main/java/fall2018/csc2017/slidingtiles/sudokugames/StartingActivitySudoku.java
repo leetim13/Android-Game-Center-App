6ee02322df182a5be67d3c086af0fea54f166da6
leetim13 <timothyhw@gmail.com>
2018-11-20 17:12:21 -0500

    added starting activity for sudoku game

---
+++ Phase2/GameCenter/app/src/main/java/fall2018/csc2017/slidingtiles/sudokugames/StartingActivitySudoku.java
@@ -0,0 +1,15 @@
+package fall2018.csc2017.slidingtiles.sudokugames;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import fall2018.csc2017.slidingtiles.R;
+
+public class StartingActivitySudoku extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_starting_sudoku);
+    }
+}
