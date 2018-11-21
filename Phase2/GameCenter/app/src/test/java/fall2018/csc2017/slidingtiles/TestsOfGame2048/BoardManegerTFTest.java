2894fa98b37c9d3c48131a221eb0d3c74df107f2
liquanzh <quanzhou.li@mail.utoronto.ca>
2018-11-20 20:10:12 -0500

    BoardManagerTF downOperation test

--- Phase2/GameCenter/app/src/test/java/fall2018/csc2017/slidingtiles/TestsOfGame2048/BoardManegerTFTest.java
+++ Phase2/GameCenter/app/src/test/java/fall2018/csc2017/slidingtiles/TestsOfGame2048/BoardManegerTFTest.java
@@ -83,4 +83,25 @@ public class BoardManegerTFTest {
         assertEquals(0, (boardManagerTF.getBoardTF().getTile(0, 1)).getId());
         assertEquals(0, (boardManagerTF.getBoardTF().getTile(1, 2)).getId());
     }
+
+    @Test
+    public void testdownOperation(){
+        boardManagerTF = setBoardManagerTF();
+        boardManagerTF.getBoardTF().getTile(0, 0).setId(2);
+        boardManagerTF.getBoardTF().getTile(0, 1).setId(1);
+        boardManagerTF.getBoardTF().getTile(0, 2).setId(3);
+        boardManagerTF.getBoardTF().getTile(0, 3).setId(1);
+        boardManagerTF.getBoardTF().getTile(1, 0).setId(2);
+        boardManagerTF.getBoardTF().getTile(1, 1).setId(5);
+        boardManagerTF.getBoardTF().getTile(1, 2).setId(1);
+        boardManagerTF.touchMove('d');
+        assertEquals(1, (boardManagerTF.getBoardTF().getTile(2, 1)).getId());
+        assertEquals(3, (boardManagerTF.getBoardTF().getTile(2, 2)).getId());
+        assertEquals(3, (boardManagerTF.getBoardTF().getTile(3, 0)).getId());
+        assertEquals(5, (boardManagerTF.getBoardTF().getTile(3, 1)).getId());
+        assertEquals(1, (boardManagerTF.getBoardTF().getTile(3, 2)).getId());
+        assertEquals(1, (boardManagerTF.getBoardTF().getTile(3, 3)).getId());
+        assertEquals(0, (boardManagerTF.getBoardTF().getTile(0, 3)).getId());
+        assertEquals(0, (boardManagerTF.getBoardTF().getTile(1, 1)).getId());
+    }
 }
