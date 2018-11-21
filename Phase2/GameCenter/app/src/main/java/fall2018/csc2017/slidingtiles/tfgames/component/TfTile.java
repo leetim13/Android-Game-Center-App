06d8b9d6d6327c9c7964812821ac64152740057a
liquanzh <quanzhou.li@mail.utoronto.ca>
2018-11-20 21:08:54 -0500

    Add javadoc for TfTile

--- Phase2/GameCenter/app/src/main/java/fall2018/csc2017/slidingtiles/tfgames/component/TfTile.java
+++ Phase2/GameCenter/app/src/main/java/fall2018/csc2017/slidingtiles/tfgames/component/TfTile.java
@@ -2,11 +2,29 @@ package fall2018.csc2017.slidingtiles.tfgames.component;
 
 import fall2018.csc2017.slidingtiles.component.BasicTile;
 
+/**
+ * Tile for game twenty forty-eight
+ */
 public class TfTile extends BasicTile {
+
+    /**
+     * Constructor of TfTile
+     * @param id the id for this tile
+     */
     public TfTile(int id){
         this.id = id;
     }
+
+    /**
+     * Update the id of this tile
+     * @param id new id
+     */
     public void setId(int id) {this.id = id; }
+
+    /**
+     * Return the id of this tile
+     * @return the id of this tile
+     */
     public int getId() {
         return this.id;
     }
