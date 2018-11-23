26a365fbcfa7c89af1c3152a65bfe3f648913652
liquanzh <quanzhou.li@mail.utoronto.ca>
2018-11-22 21:30:21 -0500

    add Sudoku background and constructor

--- Phase2/GameCenter/app/src/main/java/fall2018/csc2017/slidingtiles/sudokugames/component/SudokuTile.java
+++ Phase2/GameCenter/app/src/main/java/fall2018/csc2017/slidingtiles/sudokugames/component/SudokuTile.java
@@ -3,11 +3,25 @@ package fall2018.csc2017.slidingtiles.sudokugames.component;
 import fall2018.csc2017.slidingtiles.component.BasicTile;
 
 public class SudokuTile extends BasicTile {
+    public SudokuTile(int id){
+        this.id = id;
+    }
     public void setId(int id){
         this.id = id;
     }
     @Override
     public int getId() {
         return this.id;
+    }
+
+    /**
+     * Return the background id.
+     *
+     * @return the background id
+     */
+    public int getBackground() {
+        System.out.println("id is..." + id);
+        int background = id;
+        return background;
     }
 }
