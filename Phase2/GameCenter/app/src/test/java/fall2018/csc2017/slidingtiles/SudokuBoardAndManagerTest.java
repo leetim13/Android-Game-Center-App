57b05bfdf35272aa4c4125e1cdda4707212c98b7
liquanzh <quanzhou.li@mail.utoronto.ca>
2018-11-22 22:47:54 -0500

    add updateTile test

--- Phase2/GameCenter/app/src/test/java/fall2018/csc2017/slidingtiles/SudokuBoardAndManagerTest.java
+++ Phase2/GameCenter/app/src/test/java/fall2018/csc2017/slidingtiles/SudokuBoardAndManagerTest.java
@@ -2,7 +2,10 @@ package fall2018.csc2017.slidingtiles;
 
 import org.junit.Test;
 
+import java.util.Iterator;
+
 import fall2018.csc2017.slidingtiles.sudokugames.component.BoardSudoku;
+import fall2018.csc2017.slidingtiles.sudokugames.component.SudokuTile;
 import fall2018.csc2017.slidingtiles.sudokugames.manager.BoardManagerSudoku;
 
 import static org.junit.Assert.*;
@@ -27,5 +30,20 @@ public class SudokuBoardAndManagerTest {
         assertFalse(boardManagerSudoku.hasWon());
         boardManagerSudoku.setSudokuTiles();
         assertTrue(boardManagerSudoku.hasWon());
+    }
+
+    @Test
+    public void testUpdateTiles(){
+        boardManagerSudoku = setBoardManager();
+        Iterator<SudokuTile> iterator = boardManagerSudoku.getBoard().iterator();
+        SudokuTile temp;
+        int i = 0;
+        while(iterator.hasNext()){
+            temp = iterator.next();
+            if(temp.getId() == 0) break;
+            i++;
+        }
+        boardManagerSudoku.updateSudokuTiles(10, i);
+        assertFalse(boardManagerSudoku.checkBoardValidation());
     }
 }
