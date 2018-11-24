1ade1c9d37f957ff0c364a5c4e8ebc4a1ed5342c
wujunxuan <wujunxuan@igraphtech.com>
2018-11-24 17:14:37 -0500

    add trait classification

--- Phase2/GameCenter/app/src/main/java/fall2018/csc2017/slidingtiles/sudokugames/component/SudokuTile.java
+++ Phase2/GameCenter/app/src/main/java/fall2018/csc2017/slidingtiles/sudokugames/component/SudokuTile.java
@@ -37,7 +37,7 @@ public class SudokuTile extends BasicTile {
         this.isGenerated = isGenerated;
     }
 
-    public boolean getGenerate() {
+    public boolean generated() {
         return this.isGenerated;
     }
 
