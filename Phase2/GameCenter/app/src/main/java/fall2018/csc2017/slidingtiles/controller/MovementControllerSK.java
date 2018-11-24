f06e2433599d10849cfbc61cd64bcd47cc2fc608
wujunxuan <wujunxuan@igraphtech.com>
2018-11-24 14:51:14 -0500

    set up environment for the use of saduko game in controller

--- Phase2/GameCenter/app/src/main/java/fall2018/csc2017/slidingtiles/controller/MovementControllerSK.java
+++ Phase2/GameCenter/app/src/main/java/fall2018/csc2017/slidingtiles/controller/MovementControllerSK.java
@@ -6,15 +6,17 @@ import android.content.Intent;
 import android.widget.Toast;
 
 import fall2018.csc2017.slidingtiles.component.BasicBoardManager;
+import fall2018.csc2017.slidingtiles.helper.SaveScore;
 import fall2018.csc2017.slidingtiles.sudokugames.manager.BoardManagerSudoku;
 
 public class MovementControllerSK extends MovementController {
-    private BoardManagerSudoku boardManagerSK;
-
+    private BoardManagerSudoku boardManagerSK = new BoardManagerSudoku(9);
+    private SaveScore saveScore = new SaveScore();
     @Override
     public void setBoardManager(BasicBoardManager boardManager) {
         boardManagerSK = (BoardManagerSudoku) boardManager;
     }
     public void processTapMovement(Context context, int direction){
+        boardManagerSK.addScore();
     }
 }
