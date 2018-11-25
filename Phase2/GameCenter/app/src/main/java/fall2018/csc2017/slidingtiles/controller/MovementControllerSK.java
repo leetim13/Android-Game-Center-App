e18d9c065a66a610c128f22c9829ed3cce164ece
wujunxuan <wujunxuan@igraphtech.com>
2018-11-24 22:19:49 -0500

    add necessary for activating the choosing procedure of saduko

--- Phase2/GameCenter/app/src/main/java/fall2018/csc2017/slidingtiles/controller/MovementControllerSK.java
+++ Phase2/GameCenter/app/src/main/java/fall2018/csc2017/slidingtiles/controller/MovementControllerSK.java
@@ -6,8 +6,11 @@ import android.content.Intent;
 import android.widget.Toast;
 
 import fall2018.csc2017.slidingtiles.component.BasicBoardManager;
+import fall2018.csc2017.slidingtiles.component.User;
 import fall2018.csc2017.slidingtiles.helper.SaveScore;
 import fall2018.csc2017.slidingtiles.sudokugames.manager.BoardManagerSudoku;
+import fall2018.csc2017.slidingtiles.system.GameCacheSystem;
+import fall2018.csc2017.slidingtiles.system.UserPanel;
 
 public class MovementControllerSK extends MovementController {
     private BoardManagerSudoku boardManagerSK = new BoardManagerSudoku(9);
@@ -19,13 +22,18 @@ public class MovementControllerSK extends MovementController {
     }
 
     @Override
-    public void processTapMovement(Context context, int loadVal){
-        boardManagerSK.addScore();
+    public void processTapMovement(Context context, int position){
+        this.selectedPos = position;
     }
     /*
     * load the position of the selected target
     * */
-    public void loadPos(int position) {
-        this.selectedPos = position;
+    public void loadVal(Context ctx, int val) {
+        UserPanel panel = UserPanel.getInstance();
+        GameCacheSystem sys = GameCacheSystem.getInstance();
+        boardManagerSK.addScore();
+        boardManagerSK.updateSudokuTiles(val, selectedPos);
+        sys.update(panel.getName(), boardManagerSK);
+        sys.save(User.SD_GAME_INDEX, ctx);
     }
 }
