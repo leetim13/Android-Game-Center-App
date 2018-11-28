c6456fb8fe5e7ba7587a87b9d81a61b23d446cbf
wujunxuan <wujunxuan@igraphtech.com>
2018-11-27 23:50:43 -0500

    refactor starting for saduko

--- Phase2/GameCenter/app/src/main/java/fall2018/csc2017/slidingtiles/sudokugames/view/StartingActivitySudoku.java
+++ Phase2/GameCenter/app/src/main/java/fall2018/csc2017/slidingtiles/sudokugames/view/StartingActivitySudoku.java
@@ -9,6 +9,8 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import fall2018.csc2017.slidingtiles.BasicScoreBoardActivity;
+import fall2018.csc2017.slidingtiles.BasicStartingActivity;
 import fall2018.csc2017.slidingtiles.ProfileActivity;
 import fall2018.csc2017.slidingtiles.R;
 import fall2018.csc2017.slidingtiles.controller.UserRouter;
@@ -19,7 +21,7 @@ import fall2018.csc2017.slidingtiles.sudokugames.controller.BoardManagerSudoku;
 import fall2018.csc2017.slidingtiles.controller.system.GameCacheSystem;
 import fall2018.csc2017.slidingtiles.controller.system.UserPanel;
 
-public class StartingActivitySudoku extends AppCompatActivity {
+public class StartingActivitySudoku extends BasicStartingActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,128 +34,21 @@ public class StartingActivitySudoku extends AppCompatActivity {
         addProfileImageButtonListener();
     }
 
-    /**
-     * Activate Profile Image button, which goes to profile page
-     */
-    private void addProfileImageButtonListener() {
-        ImageButton pib = findViewById(R.id.ProfileImageButton);
-        pib.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                switchToProfile();
-            }
-        });
-    }
-    /**
-     * Activate Start button, which goes to settings page
-     */
-    private void addStartButtonListener() {
-        Button startButton = findViewById(R.id.StartButton);
-        startButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                switchToGame(true);
-            }
-        });
+    @Override
+    public void setView() {
+        setContentView(R.layout.activity_starting_tf);
     }
 
-    /**
-     * Activate the load button.
-     */
-    private void addLoadButtonListener() {
-        Button loadButton = findViewById(R.id.LoadButton);
-        final StartingActivitySudoku StartingActivitySudoku = this;
-        loadButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                BoardManagerSudoku boardManager = (BoardManagerSudoku) GameCacheSystem.getInstance().get(UserPanel.getInstance().getName());
-                if (boardManager != null) {
-                    makeToastLoadedText();
-                    switchToGame(false);
-                } else {
-                    final TextView invalidView = findViewById(R.id.warningText);
-                    ActivityHelper.disableButton(v, invalidView, "You don't have incomplete game undergoing!");
-                }
-            }
-        });
-    }
-
-    /**
-     * Display that a game was loaded successfully.
-     */
-    private void makeToastLoadedText() {
-        Toast.makeText(this, "Loaded Game", Toast.LENGTH_SHORT).show();
-    }
-
-    /**
-     * Activate the save button.
-     */
-    private void addSaveButtonListener() {
-        Button saveButton = findViewById(R.id.SaveButton);
-        final StartingActivitySudoku StartingActivitySudoku = this;
-        saveButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (GameCacheSystem.getInstance().get(UserPanel.getInstance().getName()) != null) {
-                    ActivityHelper.saveToFile(UserRouter.GAME_STORAGE_TF, StartingActivitySudoku, GameCacheSystem.getInstance().getData());
-                    makeToastSavedText();
-                } else {
-                    final TextView invalidView = findViewById(R.id.warningText);
-                    ActivityHelper.disableButton(v, invalidView, "You don't have incomplete game undergoing!");
-                }
-            }
-        });
-    }
-    /**
-     * Activate the scoreboard button.
-     */
-    private void addScoreboardButtonListener() {
-        Button startButton = findViewById(R.id.button_scoreboard);
-        startButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                switchToScoreboard();
-            }
-        });
-    }
-
-    /**
-     * Display that a game was saved successfully.
-     */
-    private void makeToastSavedText() {
-        Toast.makeText(this, "Game Saved", Toast.LENGTH_SHORT).show();
-    }
-
-    /**
-     * Switch to the GameActivity view to play the game.
-     */
+    @Override
     public void switchToGame() {
-        GameCacheSystem cached = GameCacheSystem.getInstance();
         Intent tmp = new Intent(this, GameActivitySudoku.class);
         GameCacheSystem.getInstance().save(getApplicationContext());
         startActivity(tmp);
     }
-    // a packed switch function to judge whether to order this game
-    public void switchToGame(boolean isNew) {
-        if (isNew) {
-            GameCacheSystem.getInstance().update(UserPanel.getInstance().getName(), null);
-        }
-        switchToGame();
-    }
 
-    /**
-     * Switch to the ScoreBoardActivity Activity view to see global scoreboard.
-     */
+    @Override
     public void switchToScoreboard(){
         Intent tmp = new Intent(this, ScoreBoardActivitySK.class);
-        startActivity(tmp);
-    }
-
-    /**
-     * Switch to the Profile Activity  view.
-     */
-    public void switchToProfile() {
-        Intent tmp = new Intent(this, ProfileActivity.class);
         startActivity(tmp);
     }
 }
