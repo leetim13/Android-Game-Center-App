1ee535f698104fc314aa43b370727de59e663b00
Yinling Luo <yinling.Ruby@gmail.com>
2018-11-30 09:17:20 -0500

    improve the javadoc for model classes for tf games

--- Phase2/GameCenter/app/src/main/java/fall2018/csc2017/slidingtiles/tfgames/model/TfScore.java
+++ Phase2/GameCenter/app/src/main/java/fall2018/csc2017/slidingtiles/tfgames/model/TfScore.java
@@ -10,12 +10,20 @@ import fall2018.csc2017.slidingtiles.helper.IOHelper;
 import fall2018.csc2017.slidingtiles.model.GameScore;
 import fall2018.csc2017.slidingtiles.model.component.User;
 
-/*
+/**
  * the model for the data in sliding tile score
- * */
+ */
 public class TfScore extends GameScore {
+
+    /**
+     * The map stores some scores for tf game
+     */
     private Map<String, int[]> data;
+
     @SuppressWarnings("unchecked")
+    /**
+     * To new a TfScore, stores data read from file
+     */
     public TfScore(Context ctx) {
         String filePath = indexer.index(User.TF_GAME_INDEX, StorageIndexer.SCORE);
         try {
@@ -26,6 +34,9 @@ public class TfScore extends GameScore {
     }
 
     @Override
+    /**
+     * The getter for data
+     */
     public Map<String, int[]> data() {
         return data;
     }
