534513db1f403247df3de80d7bb89c1f1ba5ad2e
wujunxuan <wujunxuan@igraphtech.com>
2018-11-30 06:08:39 -0500

    add test for saving score and iohelper

---
+++ Phase2/GameCenter/app/src/test/java/fall2018/csc2017/slidingtiles/SaveScoreTest.java
@@ -0,0 +1,21 @@
+package fall2018.csc2017.slidingtiles;
+import android.content.Context;
+
+import org.junit.Test;
+
+import fall2018.csc2017.slidingtiles.helper.SaveScore;
+
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
+
+public class SaveScoreTest {
+    // test the function of score saving
+    @Test
+    public void testSaveScore() {
+
+        SaveScore sc = new SaveScore();
+        Context ctx = mock(Context.class);
+        sc.saveScoreIntoMap(ctx, 1, 1);
+
+    }
+}
