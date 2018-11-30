a38e1f907180d1bd7c68082cda207b9b098cf3c8
wujunxuan <wujunxuan@igraphtech.com>
2018-11-30 06:03:52 -0500

    add image tile test

---
+++ Phase2/GameCenter/app/src/test/java/fall2018/csc2017/slidingtiles/ImageTileTest.java
@@ -0,0 +1,21 @@
+package fall2018.csc2017.slidingtiles;
+
+import android.graphics.Bitmap;
+
+import org.junit.Test;
+
+import fall2018.csc2017.slidingtiles.slidinggames.model.component.BitmapCollection;
+import fall2018.csc2017.slidingtiles.slidinggames.model.component.ImageTile;
+
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
+
+public class ImageTileTest {
+    @Test
+    public void testImageTile() {
+        BitmapCollection.getInstance().loadImage(new Bitmap[10][10]);
+        ImageTile tile = new ImageTile(0, 2, 2);
+        assertEquals(1, tile.getId());
+        assertEquals(null, tile.getBack());
+    }
+}
